0611e44252e904533f94fb31a80b90e66a2d66a5
STLWaffles <29383423+WafflesLabs@users.noreply.github.com>
2020-09-15 15:27:57 -0500

    Add tagging to managed disks

--- scripts/100_azurerm_managed_disk.sh
+++ scripts/100_azurerm_managed_disk.sh
@@ -79,7 +79,30 @@ if [ "$count" -gt "0" ]; then
         if [ "$stopt" != "null" ]; then 
             printf "\t storage_account_type = \"%s\"\n" $stopt >> $outfile
         fi
-
+        # New Tags block v2
+        tags=`echo $azr | jq ".[(${i})].tags"`
+        tt=`echo $tags | jq .`
+        tcount=`echo $tags | jq '. | length'`
+        if [ "$tcount" -gt "0" ]; then
+            printf "\t tags = { \n" >> $outfile
+            tt=`echo $tags | jq .`
+            keys=`echo $tags | jq 'keys'`
+            tcount=`expr $tcount - 1`
+            for j in `seq 0 $tcount`; do
+            k1=`echo $keys | jq ".[(${j})]"`
+                re="[[:space:]]+"
+                if [[ $k1 =~ $re ]]; then
+                    tval=`echo $tt | jq ."$k1"`
+                    tkey=`echo $k1 | tr -d '"'`
+                    printf "\t\t\"%s\" = %s \n" "$tkey" "$tval" >> $outfile
+                else
+                    tval=`echo $tt | jq .$k1`
+                    tkey=`echo $k1 | tr -d '"'`
+                    printf "\t\t%s = %s \n" $tkey "$tval" >> $outfile
+                fi
+            done
+            printf "\t}\n" >> $outfile
+        fi
 
         printf "}\n" >> $outfile
         #
